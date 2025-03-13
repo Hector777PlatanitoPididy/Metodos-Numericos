816e0ac4d9ff5bf3342151e0ab0820f71f62683c
Hector777PlatanitoPididy <23030684@itesa.edu.mx>
2025-03-13 09:53:48 -0600

    A

---
+++ MetodosNumericos/src/main/java/com/mycompany/metodosnumericos/MetodosNumericos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metodosnumericos;
+
+/**
+ *
+ * @author hgavi
+ */
+public class MetodosNumericos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
